4e1aa77a463a6d5f35c2f49f2aa9023e4214d19f
DevMerd <businessmert.kaya@gmail.com>
2024-02-15 03:33:08 +0300

    Tema düzenlemeleri

--- Traversal.WebUI/Controllers/DestinationController.cs
+++ Traversal.WebUI/Controllers/DestinationController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
 using Traversal.Business.Abstract;
 using Traversal.Business.Concrete;
+using Traversal.Entity.Concrete;
 
 namespace Traversal.WebUI.Controllers
 {
@@ -17,6 +19,21 @@ namespace Traversal.WebUI.Controllers
         {
             var datas = await _destinationService.GetList();
             return View(datas);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DestinationDetails(int id)
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DestinationDetails(Destination destination)
+        {
+
+
+            return View();
         }
     }
 }
